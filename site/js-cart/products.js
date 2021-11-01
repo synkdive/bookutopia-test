d1de611b35a22b96dacb076ad21195dee29c0cb3
manya <manyakhater@outlook.com>
2021-11-02 02:08:44 +0530

    testing

---
+++ site/js-cart/products.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Cultish:The Language of Fanaticism",
+    desc : "Amanda Montel",
+    img : "cultish.jpg",
+    price : 1743
+  },
+  124: {
+    name : "1984",
+    desc : "George Orwell",
+    img : "1984.jpg",
+    price : 95
+  },
+  125: {
+    name : "The Cruel Prince",
+    desc : "Holly Black",
+    img : "cruel prince.jpg",
+    price : 259
+  },
+  126: {
+    name : "The Social Instinct",
+    desc : "Nichola Raihani",
+    img : "social instinct.jpg",
+    price : 400
+  }
+};
